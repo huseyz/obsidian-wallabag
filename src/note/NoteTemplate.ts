a6e9ff73de98dd691e27aa060d894005aa512a82
jefbea <jfrancoisbeaulieu2@hotmail.com>
2023-04-01 16:56:21 -0400

    Add obsidian template variables

--- src/note/NoteTemplate.ts
+++ src/note/NoteTemplate.ts
@@ -15,7 +15,10 @@ export default class NoteTemplate {
       '{{wallabag_link}}': `${serverBaseUrl}/view/${wallabagArticle.id}`,
       '{{content}}': convertHtmlToMarkdown === 'true' ? htmlToMarkdown(wallabagArticle.content) : wallabagArticle.content,
       '{{pdf_link}}': pdfLink,
-      '{{tags}}': wallabagArticle.tags.join(', ')
+      '{{tags}}': wallabagArticle.tags.join(', '),
+      '{{reading_time}}': wallabagArticle.readingTime,
+      '{{preview_picture}}': wallabagArticle.previewPicture,
+      '{{domain_name}}': wallabagArticle.domainName
     };
     let content = this.content;
     Object.keys(variables).forEach((key) => {
